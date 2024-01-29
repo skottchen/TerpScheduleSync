6527190dd0ed3bb49aea94f58f944d6992120d44
Scott Chen <96890985+skottchen@users.noreply.github.com>
2024-01-29 15:14:15 -0500

    Add functionality to delete incorrect event instances on first day of semester

--- schedule-importer.js
+++ schedule-importer.js
@@ -1,20 +1,39 @@
 const API_KEY = "AIzaSyBG641ft-K02Iw3uLY4ALnV4bMj3CNXuB8";
 let calendarId;
 let currSemester;
+
 chrome.runtime.onMessage.addListener(async function (request) {
     if (request.action === 'performTasksAfterAuthorization') {
         const token = request.token;
-        await createNewCalendar(token);
         await getScheduleData(token);
-        window.close();//close Testudo (current window)
+        await new Promise((r) => setTimeout(r, 2000));
+        await deleteFirstDayIncorrectInstances(token);
+        // window.close();//close Testudo (current window)
         window.open("https://calendar.google.com/calendar/u/0/r") //end of application
     }
 });
 
+async function getScheduleData(token) {
+    await new Promise((r) => setTimeout(r, 3000)); //wait for Testudo to load as it is often under heavy traffic
+    currSemester = document.querySelector("span.header-dropdown-label").innerText;
+    let studentCourses = document.querySelectorAll(".course-card-container--info");
+    if (studentCourses.length > 0) {
+        await createNewCalendar(token); //only create a calendar if the student is registered for any courses
+        let count = 0;
+        for (const course of studentCourses) {
+            parseCourse(studentCourses[count].innerText, token);
+            count++;
+        }
+        
+    } else {
+        alert("You are not registered for any classes this semester.");
+    }
+}
+
 async function createNewCalendar(token) {
     const apiUrl = `https://www.googleapis.com/calendar/v3/calendars?key=${API_KEY}`
     const courseCalendar = {
-        summary: "UMD Sp24 Course Schedule",
+        summary: `UMD ${currSemester} Course Schedule`,
         timeZone: "America/New_York",
     };
 
@@ -40,24 +59,9 @@ async function createNewCalendar(token) {
         console.error('Error creating new calendar:', error);
     }
 }
-async function getScheduleData(token) {
-    await new Promise((r) => setTimeout(r, 3000)); //wait for Testudo to load as it is often under heavy traffic
-    currSemester = document.querySelector("span.header-dropdown-label").innerText;
-    console.log(currSemester);
-    let studentCourses = document.querySelectorAll(".course-card-container--info");
-    if (studentCourses.length > 0) {
-        let count = 0;
-        for (const course of studentCourses) {
-            parseCourse(studentCourses[count].innerText, token);
-            count++;
-        }
-    } else {
-        alert("You are not registered for any classes this semester.");
-    }
-}
 
-function parseCourse(course, token) {//format schedule data so that is readable by the Google Calendar API
-    let courseArr = course.split("\n")
+async function parseCourse(course, token) {//format schedule data so that is readable by the Google Calendar API
+    let courseArr = course.split("\n");
     courseArr.splice(1, 1);
     courseArr[0] = courseArr[0].slice(0, courseArr[0].length - 6);//remove section number from course
     courseArr[0] = courseArr[0].trim();
@@ -94,35 +98,46 @@ function parseCourse(course, token) {//format schedule data so that is readable 
     })
 
     if (courseArr.length > 1) {
-        importCourseIntoGoogleCalendar(courseArr, token);
+        await importCourseIntoGoogleCalendar(courseArr, token);
     }
 }
 
 //format the course day and time so that it is readable by Google Calendar API
 function parseCourseDayTime(timeArr) {
-    let newArr = timeArr;
-    if (newArr[0] == "MWF") {
-        newArr[0] = "MO,WE,FR";
-    } else if (newArr[0] == "MW") {
-        newArr[0] = "MO,WE";
-    } else if (newArr[0] == "TTh") {
-        newArr[0] = "TU,TH"
-    } else if (newArr[0] == "M") {
-        newArr[0] = "MO"
-    } else if (newArr[0] == "T") {
-        newArr[0] = "TU"
-    } else if (newArr[0] == "W") {
-        newArr[0] = "WE"
-    } else if (newArr[0] == "Th") {
-        newArr[0] = "TH"
-    } else {
-        newArr[0] = "FR"
+    let formattedDaysStr = "";
+    for (let i = 0; i < timeArr[0].length; i++) {
+        if (timeArr[0].charAt(i) == "M") {
+            formattedDaysStr += "MO,";
+        } else if (timeArr[0].charAt(i) == "W") {
+            formattedDaysStr += "WE,";
+        } else if (timeArr[0].charAt(i) == "F") {
+            formattedDaysStr += "FR,";
+        } else if (
+            timeArr[0].charAt(i) == "T" &&
+            timeArr[0].charAt(i + 1) != "h"
+        ) {
+            formattedDaysStr += "TU,";
+        } else if (
+            timeArr[0].charAt(i) == "T" &&
+            timeArr[0].charAt(i + 1) == "h"
+        ) {
+            formattedDaysStr += "TH,";
+        } else {
+            continue;
+        }
     }
+    formattedDaysStr = formattedDaysStr.slice(
+        0,
+        formattedDaysStr.length - 1
+    );
 
-    newArr[1] = parseCourseTime(newArr[1]);
-    newArr[2] = parseCourseTime(newArr[2]);
-    return newArr;
+    timeArr[0] = formattedDaysStr;
+    timeArr[1] = parseCourseTime(timeArr[1]);
+    timeArr[2] = parseCourseTime(timeArr[2]);
+
+    return timeArr;
 }
+
 
 //9:30am -> 09:30:00-7:00
 //12:00pm -> 12:00:00-7:00
@@ -149,15 +164,15 @@ async function importCourseIntoGoogleCalendar(courseArr, token) {
     const colorId = (Math.floor(Math.random() * 11) + 1).toString();//each course component (lecture and discussion) will have the same background color
     courseArr.forEach(async (elem) => {
         if (elem instanceof Array) {//create an event for each lecture, discussion, or lab
-            const elemIdx = courseArr.indexOf(elem);
-            const courseFirstDay = getCourseStartDate(courseArr[elemIdx]);
-            await createEvent(token, colorId, courseFirstDay, courseArr[0], courseArr[elemIdx], courseArr[elemIdx - 1], courseArr[elemIdx + 1])
+            const elemIdx = courseArr.indexOf(elem);//idex of array with day and time of classes
+            const semesterStartDay = getSemesterStartDate();
+            await createEvent(token, colorId, semesterStartDay, courseArr[0], courseArr[elemIdx], courseArr[elemIdx - 1], courseArr[elemIdx + 1])
         }
     })
 }
 
 //courseFormat - Lecture, Discussion, or Lab
-async function createEvent(token, colorId, courseFirstDay, courseName, courseTime, courseFormat, courseLocation) {
+async function createEvent(token, colorId, semesterStartDay, courseName, courseTime, courseFormat, courseLocation) {
     const apiUrl = `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?key=${API_KEY}`;
     const semesterEndDate = getSemesterEndDate();
     const eventDetails = {
@@ -165,11 +180,11 @@ async function createEvent(token, colorId, courseFirstDay, courseName, courseTim
         location: courseLocation,
         colorId: colorId,
         start: {
-            dateTime: `${courseFirstDay}T${courseTime[1]}`,
+            dateTime: `${semesterStartDay}T${courseTime[1]}`,
             timeZone: 'America/New_York',
         },
         end: {
-            dateTime: `${courseFirstDay}T${courseTime[2]}`,
+            dateTime: `${semesterStartDay}T${courseTime[2]}`,
             timeZone: 'America/New_York',
         },
         recurrence: [
@@ -198,29 +213,149 @@ async function createEvent(token, colorId, courseFirstDay, courseName, courseTim
     }
 }
 
-function getCourseStartDate(courseDayTimeArr) {
-    let startDate;
-    if (currSemester == "Spring 2024") {
-        if (courseDayTimeArr[0] == "MO,WE,FR" || courseDayTimeArr[0] == "MO,WE"
-            || courseDayTimeArr[0] == "WE") {
-            startDate = "2024-01-24"
-        } else if (courseDayTimeArr[0] == "TU,TH" || courseDayTimeArr[0] == "TH") {
-            startDate = "2024-01-25"
-        } else if (courseDayTimeArr[0] == "FR") {
-            startDate = "2024-01-26"
-        } else if (courseDayTimeArr[0] == "MO") {
-            startDate = "2024-01-29"
-        } else {//courseDayTimeArr[0] == "TU"
-            startDate = "2024-01-30";
-        }
+function getSemesterStartDate() {
+    let startDate = "";
+    if (currSemester == "Fall 2023") {
+        startDate = "2023-08-28"
+    } else if (currSemester == "Winter 2024") {
+        startDate = "2024-01-02"
+    } else if (currSemester == "Spring 2024") {
+        startDate = "2024-01-24"
+    } else if (currSemester == "Summer I 2024") {
+        startDate = "2024-05-28"
+    } else if (currSemester == "Summer II 2024") {
+        startDate = "2024-07-08"
+    } else if (currSemester == "Fall 2024") {
+        startDate = "2024-08-26"
+    } else if (startDate == "Winter 2025") {
+        startDate = "2025-01-02"
+    } else if (startDate == "Spring 2025") {
+        startDate = "2025-01-27"
     }
     return startDate;
 }
 
-function getSemesterEndDate(){
-    let semesterEndDate;
-    if(currSemester == "Spring 2024"){
+function getSemesterEndDate() {
+    let semesterEndDate = "";
+    if (currSemester == "Fall 2023") {
+        semesterEndDate = "20231212"
+    } else if (currSemester == "Winter 2024") {
+        semesterEndDate = "20240123"
+    } else if (currSemester == "Spring 2024") {
         semesterEndDate = "20240510"
+    } else if (currSemester == "Summer I 2024") {
+        semesterEndDate = "20240706"
+    } else if (currSemester == "Summer II 2024") {
+        semesterEndDate = "20240817"
+    } else if (currSemester == "Fall 2024") {
+        semesterEndDate = "20241210"
+    } else if (startDate == "Winter 2025") {
+        semesterEndDate = "20250123"
+    } else if (startDate == "Spring 2025") {
+        semesterEndDate = "20250514"
     }
     return semesterEndDate;
+}
+
+//the purpose of this function is to delete the instances of courses/dicussions in the
+//first day of the semester that shouldn't be there
+async function deleteFirstDayIncorrectInstances(token) {
+    const firstDayStart = getSemesterStartDate() + "T07:00:00-05:00"; // 7 am EST
+    const firstDayEnd = getSemesterStartDate() + "T22:00:00-05:00"; // 10 pm EST
+    const apiUrl = `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?key=${API_KEY}&timeMin=${firstDayStart}&timeMax=${firstDayEnd}`;
+    try {
+        const response = await fetch(apiUrl, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer ' + token,
+            },
+        });
+
+        if (response.ok) {
+            const eventData = await response.json();
+            const firstDayEvents = eventData.items;
+            const firstDayIdx = new Date(firstDayStart).getDay();
+            const dayNames = ["SUN", "MO", "TU", "WE", "TH", "FR", "SAT"];
+            const firstDay = dayNames[firstDayIdx];
+
+            for (const event of firstDayEvents) {//iterate through each event on the first day of the semester
+                let recurStr = event.recurrence[0].toString();
+                const subStr = "BYDAY";
+                const subStrIdx = recurStr.indexOf(subStr);
+                const byDayStr = recurStr.substr(subStrIdx);
+                if (!byDayStr.includes(firstDay)) {
+                    await getFirstInstance(token, event.id);
+                }
+            }
+        } else {
+            const errorData = await response.json();
+            console.error('Failed to fetch events:', errorData.error.message);
+        }
+    } catch (error) {
+        console.error('Error fetching events:', error);
+    }
+}
+
+async function getFirstInstance(token, eventId) {//necessary to get the instanceId
+    const apiUrl = `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events/${eventId}/instances?maxResults=1&key=${API_KEY}`;
+    try {
+        const response = await fetch(apiUrl, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer ' + token,
+            },
+        });
+
+        if (response.ok) {
+            const eventData = await response.json();
+            const firstInstance = eventData.items[0];
+            const firstInstanceStartTime = firstInstance.start.dateTime;
+            const firstInstanceEndTime = firstInstance.end.dateTime;
+            const instanceId = firstInstance.id;
+            await cancelFirstInstance(token, instanceId, firstInstanceStartTime, firstInstanceEndTime);
+        } else {
+            const errorData = await response.json();
+            console.error('Failed to fetch instances:', errorData.error.message);
+        }
+    } catch (error) {
+        console.error('Error fetching instances:', error);
+    }
+}
+
+async function cancelFirstInstance(token, instanceId, startTime, endTime) {
+    const apiUrl = `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events/${instanceId}?key=${API_KEY}`;
+    const eventToDelete = {
+        start: {
+            dateTime: startTime,
+            timeZone: 'America/New_York',
+        },
+        end: {
+            dateTime: endTime,
+            timeZone: 'America/New_York',
+        },
+        status: 'cancelled'
+    }
+
+    try {
+        const response = await fetch(apiUrl, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer ' + token,
+            },
+            
+            body: JSON.stringify(eventToDelete)
+        });
+
+        if (response.ok) {
+            console.log('Instance cancelled successfully:', instanceId);
+        } else {
+            const errorData = await response.json();
+            console.error('Failed to cancel instance:', errorData.error.message);
+        }
+    } catch (error) {
+        console.error('Error cancelling instance:', error);
+    }
 }
